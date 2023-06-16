2217407004e3949721d8ad10011528d0d0c37a3e
Cláudio Melgaço <claudio.melgaco@ape11.com.br>
2023-06-16 18:56:08 -0300

    requisito 1

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,17 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import (Ingredient, Restriction,)  # noqa: F401, E261, E501
 
 
-# Req 1
 def test_ingredient():
-    pass
+    bacon = Ingredient("bacon")
+    assert bacon.name == "bacon"
+    assert bacon.restrictions == {Restriction.ANIMAL_MEAT}
+    assert repr(bacon) == "Ingredient('bacon')"
+
+    test_bacon = Ingredient("bacon")
+    assert bacon.__hash__() == test_bacon.__hash__()
+    assert bacon == bacon
+
+    test_farinha = Ingredient("farinha")
+    assert bacon.__hash__() == test_farinha.__hash__()
+    assert bacon != test_farinha
+
