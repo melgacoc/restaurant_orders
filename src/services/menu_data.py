a54d276e57987b1de8b5d64d27b4db7620652b49
Cláudio Melgaço <claudio.melgaco@ape11.com.br>
2023-06-20 17:06:42 -0300

    requisito 3

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,23 @@
-# Req 3
+import pandas as pd
+from src.models.dish import Dish
+from src.models.ingredient import Ingredient
+
+
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        self.dishes = set()
+
+        for dish_name, dish_rows in (
+            pd.read_csv(source_path).drop_duplicates().groupby("dish")
+        ):
+            ingredients = {}
+            price = dish_rows["price"].iloc[0]
+            dish_description = Dish(dish_name, price)
+            for row in dish_rows.to_dict("records"):
+                ingredient_name = row["ingredient"]
+                if ingredient_name not in ingredients:
+                    ingredients[ingredient_name] = Ingredient(ingredient_name)
+                ingredient = ingredients[ingredient_name]
+                amount = row["recipe_amount"]
+                dish_description.add_ingredient_dependency(ingredient, amount)
+            self.dishes.add(dish_description)
