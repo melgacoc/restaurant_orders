60be978723866fcb2ffb2625feb23ea30eb945aa
Cláudio Melgaço <claudio.melgaco@ape11.com.br>
2023-06-20 17:23:59 -0300

    requisito 4

--- src/services/menu_builder.py
+++ src/services/menu_builder.py
@@ -3,6 +3,7 @@ import pandas as pd
 from services.inventory_control import InventoryMapping
 from services.menu_data import MenuData
 
+
 DATA_PATH = "data/menu_base_data.csv"
 INVENTORY_PATH = "data/inventory_base_data.csv"
 
@@ -24,6 +25,32 @@ class MenuBuilder:
 
         self.inventory.consume_recipe(curr_dish.recipe)
 
-    # Req 4
+    def __get_ingredients(self, recipe: dict) -> list:
+        return [ingredient for ingredient in recipe]
+
+    def __get_restrictions(self, recipe: dict[dict]) -> list:
+        return list(
+            set(
+                restriction
+                for ingredient in recipe
+                for restriction in ingredient.restrictions
+            )
+        )
+
     def get_main_menu(self, restriction=None) -> pd.DataFrame:
-        pass
+        items = self.menu_data.dishes
+        dishes = [
+            {
+                "dish_name": dish.name,
+                "price": dish.price,
+                "ingredients": self.__get_ingredients(dish.recipe),
+                "restrictions": self.__get_restrictions(dish.recipe),
+            }
+            for dish in items
+            if (
+                restriction not in set(self.__get_restrictions(dish.recipe))
+                and self.inventory.check_recipe_availability(dish.recipe)
+            )
+        ]
+
+        return pd.DataFrame(dishes)
